e21168e02069f77ea2a2f943441165b07b0773af
Tw212 <Tewe07001@utb.vaxjo.se>
2024-12-06 13:55:59 +0100

    dfaw

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Up1();
+ static void Up1()
+{
+    List<double> lista = new List<double>();
+    lista.Add(5);
+    lista.Add(6.5);
+    lista.Add(5.2);
+    lista.Add(5.4);
+    lista.Add(5.6);
+    lista.RemoveAt(2);
+    foreach(double element in lista)
+        Console.WriteLine(element);
+}
+double Medel(){
+    foreach(double element in lista)
+        Console.WriteLine(element);
+}
